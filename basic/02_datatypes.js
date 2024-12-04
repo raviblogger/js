fc694c0822ebd6c52df8182092a02091f627a92d
raviblogger <raviblogger2@gmail.com>
2024-12-04 04:21:07 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,22 @@
+
+// "use strict"; // use strict rule as per new js standard
+
+// String // "ravi"
+let name ="ravi";
+// number // two to the power 53
+let number = 1244;
+// boolean 
+let isLoggedIn = true;
+// bigint // handle big numbers
+// null //standalone value / object
+// undefined // never defined 
+let shyam;
+// symbol = unique identity
+// object 
+
+console.table([name, number, isLoggedIn, shyam]);
+
+// to know datatype 
+
+console.log(typeof null);
+console.table([typeof name, undefined, null]);
