bba6348bb97f7cb123db4ef4b442ccbf1d8261b4
raviblogger <raviblogger2@gmail.com>
2024-12-04 03:30:03 +0000

    #

---
+++ basic/01_variable.js
@@ -0,0 +1,11 @@
+const name = "ravi";
+let marks = 345
+var hobby = "cricket";
+subject = "english";
+
+// name = "shyam"; it is not allowed
+marks = 500;
+hobby = "chess";
+subject= "hindi"
+
+console.table([name, marks, hobby, subject]);
