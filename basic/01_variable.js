5902046d5552c25e1f1304d654037c24dc026c45
raviblogger <raviblogger2@gmail.com>
2024-12-04 03:33:36 +0000

    variables

--- basic/01_variable.js
+++ basic/01_variable.js
@@ -2,7 +2,7 @@ const name = "ravi";
 let marks = 345
 var hobby = "cricket";
 subject = "english";
-/* don't use var variable because it cause isse in globle and functional scope */
+/* don't use var variable because it cause isse in block and functional scope */
 // name = "shyam"; it is not allowed
 marks = 500;
 hobby = "chess";
