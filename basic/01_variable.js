ffe7c83d14c412df30e0bf341f09342965e794c6
raviblogger <raviblogger2@gmail.com>
2024-12-04 03:31:26 +0000

    variables

--- basic/01_variable.js
+++ basic/01_variable.js
@@ -2,10 +2,11 @@ const name = "ravi";
 let marks = 345
 var hobby = "cricket";
 subject = "english";
-
+/* don't use var variable because it cause isse in globle and functional scope */
 // name = "shyam"; it is not allowed
 marks = 500;
 hobby = "chess";
 subject= "hindi"
 
 console.table([name, marks, hobby, subject]);
+
