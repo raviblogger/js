0bd78baa524710da8dd87b43358cba1d16b73aa7
raviblogger <raviblogger2@gmail.com>
2024-12-13 15:28:16 +0000

    conversion of datatypes

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let num = null;
+
+// let check = Number(num);
+// console.log(typeof check);
+// console.log(check);
+
+// Number conversion and its values while changing various datatypes. 
+// number => number;
+// string => NaN
+// true => 1, false =>0
+// null => 0
+
+let isLoggedIn =0;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+// 1 => true 0=> false
+//"" => false "ravi"=> true
+
+let shyam = 1234;
+let store = String(shyam);
+console.log(store);
+console.log(typeof store);
