9714ba5092e47376c58f87b13526449670b409d4
raviblogger <raviblogger2@gmail.com>
2024-12-14 15:13:04 +0000

    conversion of datatype

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -10,14 +10,27 @@
 // true => 1, false =>0
 // null => 0
 
-let isLoggedIn =0;
-let booleanLoggedIn = Boolean(isLoggedIn);
-console.log(booleanLoggedIn);
+// let isLoggedIn =0;
+// let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
 
 // 1 => true 0=> false
 //"" => false "ravi"=> true
 
-let shyam = 1234;
-let store = String(shyam);
-console.log(store);
-console.log(typeof store);
+// let shyam = 1234;
+// let store = String(shyam);
+// console.log(store);
+// console.log(typeof store);
+
+// let num = 4;
+// let negNum = -num;
+// console.log(negNum);
+
+// let str1 = "hell";
+// let str2 = " ravi";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("2" + 2 +1); //if string is first, rest all treated as string
+console.log(2 + 2 +1); // all numbers
+console.log(2 + 2 + "1"); //before the string, rest all treated as number
