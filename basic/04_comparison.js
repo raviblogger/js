f609758d785d0f5f96032acad88e9d7cf9e78b47
raviblogger <raviblogger2@gmail.com>
2024-12-16 14:07:33 +0000

    comparison

---
+++ basic/04_comparison.js
@@ -0,0 +1,10 @@
+console.log("2">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0); //>= change null in number
+
+// ===
+
+console.log("3"===3); //it checks datatype also. 
+console.log("3"==3);
