49b6e0b74ec6cd292b74eedf2ec4c9a2457ed888
raviblogger <raviblogger2@gmail.com>
2024-12-16 14:36:41 +0000

    datatype briefly

---
+++ basic/summary_of_datatypes.js
@@ -0,0 +1,26 @@
+// primitive data types = 7
+
+// string, number, boolean, bigInt, undefined, null, symbol 
+
+let bigNumber = 6778678798798n;
+console.log(typeof bigNumber);
+
+// reference (Non - primitive datatypes) =3
+
+// array, object, function 
+
+let player = ["dhoni", "virat",];
+console.log(typeof player);
+
+
+let dhoni ={
+    name:"mahendra singh dhoni",
+    age: 38,
+    position: "wicketkeeper", 
+}
+console.log(typeof dhoni);
+
+let myCricket = function() {
+console.log("India won");
+}
+
