f635cfc84125523c3129b92adfeec15df3b3d114
raviblogger <raviblogger2@gmail.com>
2025-05-01 01:40:57 +0000

    basic variable value filling

---
+++ 1_basic/1_variables.js
@@ -0,0 +1,13 @@
+let name = "Ravi";
+const number = 12344;
+var city = "jaipur";
+state= "rajasthan";
+
+name= "Shyam";
+city = "Mumbai";
+state = "Maha"
+
+console.table([name, number, city, state])
+console.log(name)
+
+// we generally don't use var and const doesn't allow to change value. 
