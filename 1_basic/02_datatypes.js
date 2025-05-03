28ba1db66adecc33e091be60f8c744a06eb84b41
raviblogger <raviblogger2@gmail.com>
2025-05-03 05:28:19 +0000

    datatypes

---
+++ 1_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+var name = "ravi";
+let roll_no = 34;
+let consent = true;
+console.table([name, roll_no]);
+console.table([typeof name, roll_no, consent]);
+console.log(typeof consent)
+
+console.log(typeof null);
+
+// datatype 
+
+//number = 2 to power 53 
+//big 
+//null - standalone value - am object
+//boolean - true/false
+//string 
+//object 
+//undefined 
