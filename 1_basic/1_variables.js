d7620ff589a3321c625ce25ebb9bf90d6d3e85c0
raviblogger <raviblogger2@gmail.com>
2025-05-03 05:34:53 +0000

    datatypes

--- 1_basic/1_variables.js
+++ 1_basic/1_variables.js
@@ -10,4 +10,6 @@ state = "Maha"
 console.table([name, number, city, state])
 console.log(name)
 
+var msg = "hello ravi!";
+console.log(msg);
 // we generally don't use var and const doesn't allow to change value. 
---
+++ 1_basic/2_datatypes.js
@@ -0,0 +1,9 @@
+//number - 2 to power 53
+//big int 
+//null - standalone valud - an object 
+//undefined 
+//boolean -true/false
+//string - ""
+//object 
+
+console.log(typeof null);
