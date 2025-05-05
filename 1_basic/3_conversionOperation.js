118140a04cc60e8e6febaca3e992e9c8fd3b4888
raviblogger <raviblogger2@gmail.com>
2025-05-05 01:33:32 +0000

    datatype conversion

---
+++ 1_basic/3_conversionOperation.js
@@ -0,0 +1,29 @@
+const score = false;
+const valueinNumber = Number(score);
+console.log(typeof score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+/* Number Conversion 
+"22" = 22
+"22abc" = NaN
+true =1, false =0,
+*/
+
+let loggedIn = "";
+let log_status = Boolean(loggedIn);
+console.log(log_status);
+
+/* Boolean Conversion 
+1= true;
+0= false;
+"ravi" = true;
+"" = false; 
+*/
+
+let name = 33;
+let valueinString = String(name);
+console.log(typeof valueinString);
+
+/* 33 = String(33) => 33 as a String
+*/
