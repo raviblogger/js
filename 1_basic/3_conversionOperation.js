d14112554ac989e4cb88f59418eae22ee212c847
raviblogger <raviblogger2@gmail.com>
2025-05-10 12:01:31 +0000

    prefix and suffix

--- 1_basic/3_conversionOperation.js
+++ 1_basic/3_conversionOperation.js
@@ -1,8 +1,8 @@
 const score = false;
 const valueinNumber = Number(score);
-console.log(typeof score);
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
+// console.log(typeof score);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
 
 /* Number Conversion 
 "22" = 22
@@ -12,7 +12,7 @@ true =1, false =0,
 
 let loggedIn = "";
 let log_status = Boolean(loggedIn);
-console.log(log_status);
+//console.log(log_status);
 
 /* Boolean Conversion 
 1= true;
@@ -23,7 +23,19 @@ console.log(log_status);
 
 let name = 33;
 let valueinString = String(name);
-console.log(typeof valueinString);
+//console.log(typeof valueinString);
 
 /* 33 = String(33) => 33 as a String
 */
+
+
+/*prefix*/
+
+let n1= 3;
+let n2= ++n1;
+console.log(`n1:${n1}, n2:${n2} `)
+
+/*suffix*/
+let n3 = 4;
+let n4 = n3++;
+console.log(`n3:${n3}, n4:${n4}`);
