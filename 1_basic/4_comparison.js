3aafba618f50d718731a03904f9c7920ca853f17
raviblogger <raviblogger2@gmail.com>
2025-05-11 03:57:29 +0000

    comparison values

---
+++ 1_basic/4_comparison.js
@@ -0,0 +1,12 @@
+console.log(null >0);
+console.log(null == 0);
+console.log(null>= 0);
+// > convert null into 0, then 0=0 is true 
+
+
+console.log(undefined > 0);
+console.log(undefined== 0);
+console.log(undefined < 0);
+
+console.log(1===1);
+console.log("1"===1);
